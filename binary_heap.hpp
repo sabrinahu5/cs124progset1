a02d38e23dcdcebe5d6670cd8a12cd135b8ace38
Sabrina Hu <yourgitemail@example.com>
2023-02-23 11:15:13 -0500

    changed to c++, created heap class

---
+++ binary_heap.hpp
@@ -0,0 +1,91 @@
+#include <vector>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// Node structure (for each vertex)
+struct Node {
+  int ID;
+  int type;
+  double value;
+  double coords[4];
+};
+
+// Construct MinHeap class
+class MinHeap {
+
+    private:
+
+        vector<Node> heap;
+
+        int parent(int i) {
+            return (i-1)/2;
+        }
+
+        int left(int i) {
+            return 2*i + 1;
+        }
+
+        int right(int i) {
+            return 2*i + 2;
+        }
+
+        void swap(Node& x, Node& y) {
+            Node temp = x;
+            x = y;
+            y = temp;
+        }
+
+        void minHeapify(int i) {
+            int l = left(i);
+            int r = right(i);
+            int smallest = i;
+
+            if (l < heap.size() && heap[l].value < heap[smallest].value) {
+                smallest = l;
+            }
+
+            if (r < heap.size() && heap[r].value < heap[smallest].value) {
+                smallest = r;
+            }
+
+            if (smallest != i) {
+                swap(heap[i], heap[smallest]);
+                minHeapify(smallest);
+            }
+        }
+
+    public:
+
+        int size() {
+            return heap.size();
+        }
+
+        void insert(Node n) {
+            heap.push_back(n);
+
+            int i = heap.size() - 1;
+            while (i != 0 && heap[parent(i)].value > heap[i].value) {
+                swap(heap[i], heap[parent(i)]);
+                i = parent(i);
+            }
+        }
+
+        Node getMin() {
+            if (heap.empty()) {
+                return {0, 0, 0, {0,0,0,0}};
+            }
+
+            Node minNode = heap[0];
+
+            heap[0] = heap.back();
+            heap.pop_back();
+
+            minHeapify(0);
+
+            return minNode;
+        }
+
+
+};
---
+++ mst.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Produces random edge
+double random_double() {
+    return ((double)rand() / (double) RAND_MAX);
+}
+
+// Gets distance between two points (for dim 2, 3, 4)
+double get_dist(int u, int v) {
+
+}
+
+
+int main(int argc, char** argv) {
+    
+    return 0;
+
+
+}
